8d4e5cf46c27355aeabea3c137b630802a24ce92
Faizan Talha <faizansannan3@gmail.com>
2026-03-19 23:21:59 +0500

    Added Icon and Finally Started working on the actual game. I have a lot of work to do, but I am excited to get started!

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,32 @@
 #include "raylib.h"
+#include <iostream>
+using namespace std;
 
-int main() {
-    InitWindow(1280, 720, "RigBuilder Tycoon");
+int main(){
+    // Initialize the window with a custom width, height, and an emoji title
+    InitWindow(1280,720,"🖥️ RigBuilder Tycoon 🛠️");
+    
+    // Load the Icon Image File and set it as the window icon,then unload the image from memory
+    Image icon = LoadImage("icon.png"); 
+    SetWindowIcon(icon); 
+    UnloadImage(icon); 
+
+    // Lock the game to run at a smooth 60 FPS
     SetTargetFPS(60);
 
+
+    // Keep running the game loop until the user presses the X button or hits ESC
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("RigBuilder Tycoon Environment is Setup!", 350, 340, 30, DARKGRAY);
+        
+        // Draw some text in the middle of the screen to prove it works
+        DrawText("RigBuilder Tycoon Environment is Setup!",350,340,30,DARKGRAY);
+        
         EndDrawing();
     }
 
+    // Safely close the window and clean up any leftover background resources
     CloseWindow();
     return 0;
 }
